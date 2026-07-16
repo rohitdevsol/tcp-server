e5600f73a6a255b613161b541960095df71c36a3
Rohit <rohitnarnolia88@gmail.com>
2026-07-16 09:52:49 +0530

    fix: less than 0 clippy issue

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,9 @@ use tokio::net::TcpStream;
 #[tokio::main]
 async fn main() {
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-
-
     loop {
         let (mut stream, socket_addr) = listener.accept().await.unwrap();
-       let _ = tokio::spawn( async move {
+       tokio::spawn( async move {
             process_stream(&mut stream).await
         });
 
@@ -22,9 +20,10 @@ async fn process_stream(stream:&mut TcpStream) {
 
     loop {
         let res = stream.read(&mut buf).await.unwrap();
-        if res <=0 {
+        if res ==0 {
             break;
-        }
+        } // this is here to deal with the FIN packet thingy i.e 0
+
         // now I know there are res number of bytes that are read
         println!("The client sent this message {:?}", String::from_utf8_lossy(&buf[..res]));
         stream.write_all(&buf[..res]).await.unwrap();

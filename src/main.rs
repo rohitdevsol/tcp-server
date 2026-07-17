0795cc84d4bb75b5801398b6bd2a208d636d7148
Rohit <rohitnarnolia88@gmail.com>
2026-07-17 13:59:26 +0530

    small refactor

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,20 @@ use tokio::sync::broadcast;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    // we will make a map here
-    // let clients = Clients::new(Mutex::new(HashMap::new()));
-
     let (tx, _) = broadcast::channel::<Broadcast>(16);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+
     loop {
         if let Ok((stream, addr)) = listener.accept().await {
             println!("Got a new connection from client : {}", addr);
+
             let (read_half, write_half) = stream.into_split();
+
+            let (shutdown_tx, shutdown_rx) = tokio::sync::oneshot::channel::<()>();
 
             let rx = tx.subscribe();
             let tx_clone = tx.clone();
-            let (shutdown_tx, shutdown_rx) = tokio::sync::oneshot::channel::<()>();
 
             tokio::spawn(async move {
                 let _ = socket_reader_loop(read_half, tx_clone, addr).await;
